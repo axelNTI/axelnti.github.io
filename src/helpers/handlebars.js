62d0316cfde9e3fc5d7e99f8ed1efe92428abf19
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-11-06 14:34:05 +0100

    Removed old unused handlebars helper function

--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -1,14 +1,11 @@
 const translate = (key, locale) => {
    return locale[key] || key;
 };
-
-const formatNumber = (phoneNumber, locale, location) => {
-   const language = locale.language;
-   const localizedPhoneNumber = language === "sv" ? phoneNumber.replace("+46 ", "0") : phoneNumber;
-   return location === "href" ? localizedPhoneNumber.replace(/[^0-9+]/g, "") : localizedPhoneNumber;
+const format = (number) => {
+   return number.replace(/[^\d+]/g, "");
 };
 
 module.exports = {
    translate,
-   formatNumber,
+   format,
 };
