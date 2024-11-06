774bb4d0bb8dbb71d5426d04d5b9d1eb6ea26122
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2024-11-06 09:47:53 +0100

    Copied handlebars helper functions from old project

---
+++ src/helpers/handlebars.js
@@ -0,0 +1,14 @@
+const translate = (key, locale) => {
+   return locale[key] || key;
+};
+
+const formatNumber = (phoneNumber, locale, location) => {
+   const language = locale.language;
+   const localizedPhoneNumber = language === "sv" ? phoneNumber.replace("+46 ", "0") : phoneNumber;
+   return location === "href" ? localizedPhoneNumber.replace(/[^0-9+]/g, "") : localizedPhoneNumber;
+};
+
+module.exports = {
+   translate,
+   formatNumber,
+};
