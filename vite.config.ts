2d8a720857b434bbd29753e52942fc4b9d5ed980
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-14 11:48:09 +0200

    Added unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import preact from "@preact/preset-vite";
+import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    UnoCSS(),
     preact({
       prerender: {
         enabled: true,
