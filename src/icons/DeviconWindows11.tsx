3aab388a4758bb2ce3535e03ce408ad81e6bde39
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-15 13:56:07 +0200

    Added tech icons

---
+++ src/icons/DeviconWindows11.tsx
@@ -0,0 +1,21 @@
+import type { JSX } from "preact";
+
+export function DeviconWindows11(props: JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 128 128"
+      {...props}
+    >
+      <title>Windows 11</title>
+      {/* Icon from Devicon by konpa - https://github.com/devicons/devicon/blob/master/LICENSE */}
+      <path
+        fill="#0078d4"
+        d="M67.328 67.331h60.669V128H67.328zm-67.325 0h60.669V128H.003zM67.328 0h60.669v60.669H67.328zM.003 0h60.669v60.669H.003z"
+      />
+    </svg>
+  );
+}
+export default DeviconWindows11;
---
+++ src/icons/MaterialIconThemeBiome.tsx
@@ -0,0 +1,21 @@
+import type { JSX } from "preact";
+
+export function MaterialIconThemeBiome(props: JSX.SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 74 74"
+      {...props}
+    >
+      <title>Biome.js</title>
+      {/* Icon from Material Icon Theme by Material Extensions - https://github.com/material-extensions/vscode-material-icon-theme/blob/main/LICENSE */}
+      <path
+        fill="#42A5F5"
+        d="M37 9L22.745 33.69a32.2 32.2 0 0 1 16.869-.584l4.818 1.137l-4.533 19.22l-4.825-1.137c-5.93-1.399-11.628 1.716-14.036 6.685l-4.46-2.158c3.404-7.029 11.425-11.285 19.637-9.347l2.259-9.58A27.23 27.23 0 0 0 5 64.424l64 .001z"
+      />
+    </svg>
+  );
+}
+export default MaterialIconThemeBiome;
