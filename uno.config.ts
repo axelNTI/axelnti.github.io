08e80c5680141cd75ccbb02f7d17c0db5f72a475
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-15 16:02:18 +0200

    Setup unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,32 @@
-import { defineConfig } from "unocss";
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetWebFonts,
+  presetWind3,
+  transformerAttributifyJsx,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
 
 export default defineConfig({
-  // ...UnoCSS options
+  presets: [presetWind3(), presetAttributify(), presetIcons(), presetTypography(), presetWebFonts()],
+  transformers: [transformerDirectives(), transformerVariantGroup(), transformerAttributifyJsx()],
+  theme: {
+    animation: {
+      keyframes: {
+        float: "{0%{transform:translateY(0px)}50%{transform:translateY(-20px)}100%{transform:translateY(0px)}}",
+      },
+      durations: {
+        float: "3s",
+      },
+      timingFns: {
+        float: "ease-in-out",
+      },
+      counts: {
+        float: "infinite",
+      },
+    },
+  },
 });
