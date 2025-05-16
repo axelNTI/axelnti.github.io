2bd1e5419bf533a8bdb47664ef83dcec27b41cff
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-16 14:59:14 +0200

    Removed unused unocss config

--- uno.config.ts
+++ uno.config.ts
@@ -13,20 +13,5 @@ import {
 export default defineConfig({
   presets: [presetWind3(), presetAttributify(), presetIcons(), presetTypography(), presetWebFonts()],
   transformers: [transformerDirectives(), transformerVariantGroup(), transformerAttributifyJsx()],
-  theme: {
-    animation: {
-      keyframes: {
-        float: "{0%{transform:translateY(0px)}50%{transform:translateY(-20px)}100%{transform:translateY(0px)}}",
-      },
-      durations: {
-        float: "3s",
-      },
-      timingFns: {
-        float: "ease-in-out",
-      },
-      counts: {
-        float: "infinite",
-      },
-    },
-  },
+  theme: {},
 });
