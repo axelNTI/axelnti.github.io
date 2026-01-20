39f844392e1521a69bf2cafdc32282873b891e9f
Axel Thornberg <axel@thornberg.se>
2026-01-20 10:01:32 +0100

    Removed unused dependency

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,7 +1,12 @@
 import { copyFileSync } from "node:fs";
 
 const main = () => {
-  copyFileSync("dist/index.html", "dist/404.html");
+  try {
+    copyFileSync("dist/index.html", "dist/404.html");
+  } catch (e) {
+    console.error(`Error copying 404 html file: ${e}`);
+    process.exit(1);
+  }
 };
 
 main();
